3859ebd042f664cd56a3ec804dbf389b64b5479e
jeffryanfl <fl.jeff.ryan@gmail.com>
2026-08-20 11:46:11 -0400

    Streamline site to minimalist editorial writing canvas and archive subpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: true,
+  },
+  preview: {
+    host: true,
+  },
+  vite: {
+    server: {
+      allowedHosts: true,
+    },
+    preview: {
+      allowedHosts: true,
+    },
+  },
+});
